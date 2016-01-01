5cc99b67074b1f47b7561a2ea8035f84ce2afbd9
Yoshinori.N <nayo-msoft@outlook.jp>
2016-01-01 21:29:27 +0900

    (Issue #14):テキストボックスフォーカス時のビヘイビアを作成し、フォーカス時に項目を全選択するようにした。

---
+++ DecimalConverter/Behavior/TextBoxSelectBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace DecimalConverter.Behavior
+{
+    /// <summary>
+    /// テキストボックスフォーカス時のビヘイビア
+    /// </summary>
+    public class TextBoxSelectBehavior : Behavior<TextBox>
+    {
+        private Random random = new Random();
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.GotFocus += AssociatedObject_GotForcus;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.GotFocus -= AssociatedObject_GotForcus;
+        }
+
+        private void AssociatedObject_GotForcus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            //入力されている全ての文字を選択する。
+            textBox.SelectAll();
+        }
+    }
+}
