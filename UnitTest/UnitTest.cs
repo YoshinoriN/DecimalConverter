891ccdbb7c575026ce31bae92052313cc7ae45f2
Yoshinori.N <nayo-msoft@outlook.jp>
2016-01-01 22:08:20 +0900

    とりあえず、仮レベルでテストコード書いてみた。

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecimalConverter;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+
+        private DecimalConverter.ViewModels.MainWindowViewModel vm = new DecimalConverter.ViewModels.MainWindowViewModel();
+
+        /// <summary>
+        /// 10進数に値を入れたときのテスト
+        /// </summary>
+        [TestMethod, TestCategory("ViewModelProperty")]
+        public void TestDecimalNumber()
+        {
+            vm.DecimalNumber = 0;
+            Assert.AreEqual("0", vm.BinaryNumber, "2進数:0");
+            Assert.AreEqual("0", vm.OctDecimalNumber, "8進数:0");
+            Assert.AreEqual("0", vm.HexaDecimalNumber, "16進数:0");
+        }
+    }
+}
